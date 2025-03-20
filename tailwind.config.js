7497a995de24902e38a205bdf6d7484e476f7d9b
Andromed Huda <andromedhuda@gmail.com>
2025-03-20 21:59:11 +0700

    first

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+ /** @type {import('tailwindcss').Config} */
+ export default {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        checkungu: "#874efe",
+        checkhijau: "#4f7a28",
+        checkabu: "#929292",
+        kotakcoklat: "#ffe599",
+        kotakkuning: "#fdff00",
+        garisbiru: "#2b78e4"
+      }
+    },
+  },
+  plugins: [],
+}
